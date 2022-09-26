01516e1c4618422e2e366aa048263c75cf006db7
dodo <dodo@notdodo.com>
2022-09-26 16:37:32 +0000

    First Commit

--- next.config.js
+++ next.config.js
@@ -5,3 +5,19 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+	  reactStrictMode: true,
+	  typescript: {
+		      // !! WARN !!
+		  //     // Dangerously allow production builds to successfully complete even if
+		  //         // your project has type errors.
+		  //             // !! WARN !!
+                 ignoreBuildErrors: true,
+          },
+          eslint: {
+		  //                         // Warning: This allows production builds to successfully complete even if
+		  //                             // your project has ESLint errors.
+                                 ignoreDuringBuilds: true,
+		  //                                   },
+           }
